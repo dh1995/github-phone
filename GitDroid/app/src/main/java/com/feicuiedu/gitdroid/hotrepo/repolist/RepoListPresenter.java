ed8fd06514eed42738d7196da90443cbd882ff83
denghao <577214800@qq.com>
2016-07-29 08:58:56 +0800

    更新Okhttp实现网络请求

--- GitDroid/app/src/main/java/com/feicuiedu/gitdroid/hotrepo/repolist/RepoListPresenter.java
+++ GitDroid/app/src/main/java/com/feicuiedu/gitdroid/hotrepo/repolist/RepoListPresenter.java
@@ -3,8 +3,16 @@ package com.feicuiedu.gitdroid.hotrepo.repolist;
 import android.os.AsyncTask;
 
 import com.feicuiedu.gitdroid.hotrepo.repolist.view.RepoListView;
+import com.feicuiedu.gitdroid.network.GitHubApi;
+import com.feicuiedu.gitdroid.network.GitHubClient;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by dh1 on 2016/7/27.
@@ -19,8 +27,47 @@ public class RepoListPresenter {
     }
     //下拉刷新处理
     public void refresh(){
-        new RefreshTask().execute();
+//        new RefreshTask().execute();
+        GitHubClient gitHubClient=new GitHubClient();
+        GitHubApi gitHubApi=gitHubClient.gitHubApi();
+        Call<ResponseBody> call=gitHubApi.getRetrofitContributors();
+        // #1 直接在当前线程执行
+        // #2 异步执行
+        call.enqueue(refreshCallback);
     }
+    private final Callback<ResponseBody> refreshCallback=new Callback<ResponseBody>() {
+       //响应
+        @Override
+        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+            repoListView.stopRefresh();
+            //获取成功200-299
+            if(response.isSuccessful()){
+                ResponseBody body=response.body();
+                if (body==null){
+                    repoListView.showMessage("未知错误");
+                    return;
+                }
+                try {
+                    String content=body.string();
+                    repoListView.showContentView();
+                } catch (IOException e) {
+                    onFailure(call,e);
+                }
+            }else {
+                //401:一般是指未授权
+                repoListView.showMessage("code:"+response.code());
+            }
+        }
+
+        //失败：比如网络连接异常
+        @Override
+        public void onFailure(Call<ResponseBody> call, Throwable t) {
+            repoListView.stopRefresh();
+            repoListView.showMessage(t.getMessage());
+            repoListView.showContentView();
+        }
+    };
+
     //加载更多处理
     public void LoadMore(){
         repoListView.showLoadMoreLoading();
